fef464b83c1d0fc8430d75e2d58bc95f8d834211
Harsh Zala <140790325+harshzala1@users.noreply.github.com>
2026-01-12 09:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
+
 
 app = Flask(__name__)
 
@@ -56,5 +58,7 @@ def predict():
                            text_color=result_color,
                            advice_text=advice)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
